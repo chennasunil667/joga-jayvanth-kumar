473f18a720e969c21f06a9c80d18a254ac9db164
Sunil Chenna <chennasunil667@gmail.com>
2026-07-08 22:45:52 +0530

    Improve hero layout and start mobile responsive design

--- js/script.js
+++ js/script.js
@@ -143,3 +143,10 @@ window.addEventListener("scroll",()=>{
     }
 
 });
+
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+});
